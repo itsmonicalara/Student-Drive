299cab08f06db9515574b33d43288ca568ce3252
Monica Lara <38739285+itsmonicalara@users.noreply.github.com>
2022-06-03 19:42:32 -0500

    New contracts

---
+++ contracts/Reservation.sol
@@ -0,0 +1,28 @@
+// "additional information that only the compiler cares about"
+pragma solidity ^0.5.0;
+
+contract Reservation {
+    // Solidity has a unique type called an address.
+    address[16] public riders;
+
+    // Reserving a ride
+    // We are checking to make sure driverId is in range of our rider array.
+    function reserve(uint driverId) public returns (uint) {
+      require(driverId >= 0 && driverId <= 15);
+    
+      riders[driverId] = msg.sender;
+    
+      return driverId;
+    }
+
+    // Retrieving the rider
+    // The view keyword in the function declaration means that the function will not modify the state of the contract. 
+    function getRiders() public view returns (address[16] memory) {
+      return riders;
+    }
+
+
+
+    
+}
+
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
-var Adoption = artifacts.require("Adoption");
+var Reservation = artifacts.require("Reservation");
 
 module.exports = function(deployer) {
-  deployer.deploy(Adoption);
+  deployer.deploy(Reservation);
 };
